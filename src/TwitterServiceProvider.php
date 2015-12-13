f505425d9cf2fb88036454ad55fe03c4227d6dbb
Mike Barwick <mbarwick@socialbungy.com>
2015-12-13 00:31:45 -0500

    init

---
+++ src/TwitterServiceProvider.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Mbarwick83\TwitterApi;
+
+use Illuminate\Support\ServiceProvider;
+
+class TwitterApiServiceProvider extends ServiceProvider
+{
+    /**
+     * Indicates if loading of the provider is deferred.
+     *
+     * @var bool
+     */
+    protected $defer = false;
+
+    /**
+     * Perform post-registration booting of services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->publishes([
+            __DIR__.'/config/twitterapi.php' => config_path('twitter-api.php'),
+        ], 'config');
+    }
+
+    /**
+     * Register any package services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app->bind('Mbarwick83\TwitterApi\TwitterApi',function($app){
+            return new TwitterApi($app);
+        });
+    }
+}
